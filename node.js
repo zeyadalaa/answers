02c0a529310f85f823b8c6b975c262a040527df0
zeyadalaa <47432842+zeyadalaa@users.noreply.github.com>
2021-07-27 21:25:41 +0200

    outputs

---
+++ node.js
@@ -0,0 +1,19 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+var md5 = require('md5');
+
+let resultsString= ""
+let i=1;
+
+fs.createReadStream('annual-enterprise-survey-2020-financial-year-provisional-csv.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+      i%2 ==1 ? resultsString += data['Industry_code_NZSIOC'] :false 
+      i++
+
+})
+  .on('end', () => {
+    console.log(md5(resultsString));
+    // console.log(resultsString);
+  });
+  
